178127081bb6e65db5adc5adffbc2a465933dd2f
Dunya.18 <blddounia@gmail.com>
2022-01-15 15:35:00 +0100

    commit 2

--- src/main/java/com/telly/controllers/UserController.java
+++ src/main/java/com/telly/controllers/UserController.java
@@ -12,6 +12,11 @@ public class UserController {
 	public String showLogin() {
 		return "login";
 	}
+
+	@RequestMapping("/loggedout")
+	public String showLogout() {
+		return "loggedout";
+	}
 }
 
 
