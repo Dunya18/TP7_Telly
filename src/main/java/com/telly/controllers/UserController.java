e929ee0ae0bc3c739e68c3099f629c8aef7b8f73
Dunya.18 <blddounia@gmail.com>
2022-01-19 23:32:40 +0100

    fusion

--- src/main/java/com/telly/controllers/UserController.java
+++ src/main/java/com/telly/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.telly.controllers;
 
 import java.security.Principal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,48 +12,85 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.telly.dao.FormValidationGroup;
+import com.telly.dao.Reserve;
 import com.telly.dao.User;
+import com.telly.service.ReserveService;
 import com.telly.service.UserService;
+
 
 
 @Controller
 public class UserController {
+
 	@Autowired
 	UserService userService;
-	@RequestMapping("/login")
-	public String showLogin() {
-		return "login";
-	}
 
-	@RequestMapping("/loggedout")
-	public String showLogout() {
-		return "loggedout";
-	}
-	@RequestMapping("/createaccount")
-	public String createAccount(Model model, Principal principal) {
+	@Autowired
+	ReserveService reserveService;
 
-		model.addAttribute("user", new User());
+	@RequestMapping(value = "/reservebook", method = RequestMethod.POST)
+	public String createReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, BindingResult result, Principal principal) {
 
-		return "createaccount";
-	}
-
-	@RequestMapping(value = "/createuser", method = RequestMethod.POST)
-	public String createUser(@Validated(FormValidationGroup.class) User user, BindingResult result) {
-
-		if(result.hasErrors()) {
-			return "createaccount";
+		if (result.hasErrors()) {
+			return "reservebus";
 		}
 
-		user.setAuthority("ROLE_USER");
-		user.setEnabled(true);
+		String username = principal.getName();
+		reserve.getUser().setUsername(username);
 
-		userService.create(user);
+		reserveService.reserve(reserve);
+
 
 		return "home";
 
 	}
 
-}
+	@RequestMapping(value = "/getreservations", method = RequestMethod.GET)
+	public String getReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, Model model, Principal principal) {
+
+
+		String username = principal.getName();
+		reserve.getUser().setUsername(username);
+
+		List<Reserve> reserves = reserveService.getReserves(username);
+		model.addAttribute("reserves", reserves);
+		System.out.println(reserves);
+		return "home";
+	}
+		@RequestMapping("/login")
+		public String showLogin () {
+			return "login";
+		}
+
+		@RequestMapping("/loggedout")
+		public String showLogout () {
+			return "loggedout";
+		}
+		@RequestMapping("/createaccount")
+		public String createAccount (Model model, Principal principal){
+
+			model.addAttribute("user", new User());
+
+			return "createaccount";
+		}
+
+		@RequestMapping(value = "/createuser", method = RequestMethod.POST)
+		public String createUser (@Validated(FormValidationGroup.class) User user, BindingResult result){
+
+			if (result.hasErrors()) {
+				return "createaccount";
+			}
+
+			user.setAuthority("ROLE_USER");
+			user.setEnabled(true);
+
+			userService.create(user);
+
+			return "home";
+
+		}
+
+	}
 
 
 
